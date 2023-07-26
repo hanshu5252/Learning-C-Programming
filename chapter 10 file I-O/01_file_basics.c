9939391e07d7b9d9508a0c44b47e53bfc49d7cab
Himanshu Singh <108223995+hanshu5252@users.noreply.github.com>
2023-07-26 20:17:47 +0530

    Delete chapter 10 file I-O directory

--- chapter 10 file I-O/01_file_basics.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{   FILE *ptr;
-   ptr = fopen("sample.txt","r");   // for read operation 
- //  ptr = fopen("sample.txt","w");    // for write operation
-    return 0;
-}
-// in a appnd mode if file is not exist then it creates a new file
